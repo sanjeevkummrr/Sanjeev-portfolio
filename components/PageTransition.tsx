3b1c17a6c1663f976ab9e0554ecd7f08087a5015
Sanjeev Kumar <snkumar3122000@gmail.com>
2026-08-19 18:04:17 +0530

    Add PageTransition component for page transitions

---
+++ components/PageTransition.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { ReactNode } from "react";
+import { usePathname } from "next/navigation";
+
+export default function PageTransition({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const pathname = usePathname();
+
+  return (
+    <div className="page-transition" key={pathname}>
+      {children}
+    </div>
+  );
+}
